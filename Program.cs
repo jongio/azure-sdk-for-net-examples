0986659a1cbbf33f200c6bb45310fa2f11d40a7a
Jon Gallant <jong@microsoft.com>
2021-05-13 09:37:34 -0700

    Init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Azure.Identity;
+using System.Threading.Tasks;
+
+namespace azure_sdk_for_net_examples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Get all resources in a resource group
+            var subscriptionId = "";
+            var resourceGroupName = "";
+
+            // Login with azure cli, azure psh, vscode, visual studio, before running locally.
+            var cred = new DefaultAzureCredential();
+            var client = new Azure.ResourceManager.Resources.ResourcesManagementClient(subscriptionId, cred);
+            var resources = client.Resources.ListByResourceGroupAsync(resourceGroupName);
+            await foreach (var resource in resources)
+            {
+                Console.WriteLine(resource.Name);
+            }
+        }
+    }
+}
